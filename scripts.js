2a1f5617c83463c18b55e0458598eed8029c2fda
rachelcherry <rcherry@bu.edu>
2024-02-09 13:44:26 -0500

    added comments to script file

--- scripts.js
+++ scripts.js
@@ -19,10 +19,10 @@ document.addEventListener('DOMContentLoaded', function () {
             You will need to add more code here that will also add the users email to the list
             titled "users who've signed up"
              */
-            const emailList = document.querySelector('#emailList');
-            const listItem = document.createElement('li');
-            listItem.textContent = userEmail;
-            emailList.appendChild(listItem);
+            const emailList = document.querySelector('#emailList'); // finds the item with the id emailList which is found in index.html and stores it for use later
+            const listItem = document.createElement('li'); // this creates an item for the list
+            listItem.textContent = userEmail; // sets the content (the user email) to be text 
+            emailList.appendChild(listItem); // add the item (the users email) to the list that is labeled as Users Who have signed up
 
         } else {
             alert('No email provided. Please try again if you wish to be notified.');
@@ -34,10 +34,10 @@ document.addEventListener('DOMContentLoaded', function () {
     /*
     Add an event listener for the iOS Button
      */
-    const AppleButton = document.getElementById('applebutton');
-    AppleButton.addEventListener('click', function () {
-        handleJoinClick("Apple");
-        event.preventDefault();
+    const AppleButton = document.getElementById('applebutton'); // creates a variable that holds the apple button creates in index.html
+    AppleButton.addEventListener('click', function () { // add an event listener that occurs on the click
+        handleJoinClick("Apple"); // call the handleJoinClick function with the platform labeled as Apple
+        event.preventDefault(); // prevent refresh on the click event, so when the user inputs an email it maintains the list of users
     });
     
 
@@ -45,10 +45,10 @@ document.addEventListener('DOMContentLoaded', function () {
     Add an event listener for the Android Button
      */
 
-    const AndroidButton = document.getElementById('androidbutton');
-    AndroidButton.addEventListener('click', function () {
-        handleJoinClick("Android");
-        event.preventDefault();
+    const AndroidButton = document.getElementById('androidbutton'); // create variable to hold android button
+    AndroidButton.addEventListener('click', function () { // add event listener that occurs on the click 
+        handleJoinClick("Android"); // call the handleJoinClick function with the platform labeled as Android 
+        event.preventDefault(); // prevent refresh on the click event, so when the user inputs an email it maintains the list of users
     });
     /*
     Below here you can add code for what happens when the user clicks "Sign Up"
@@ -64,19 +64,19 @@ document.addEventListener('DOMContentLoaded', function () {
      Help 3: https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input
      */
 
-  const form = document.getElementById('signUp');
-  const email = document.getElementById('emailRead');
-  form.addEventListener('submit', (event) => { /* from Mozilla */
-      event.preventDefault();
-      const inputEmail = email.value;
-      if (inputEmail) {
-        const emailList = document.querySelector('#emailList');
-        const listItem = document.createElement('li');
-        listItem.textContent = inputEmail;
-        emailList.appendChild(listItem);
-        email.value = '';
+  const form = document.getElementById('signUp'); // creates a variable that holds the sign up button 
+  const email = document.getElementById('emailRead'); // creates a variable that holds the list for users who have signed up 
+  form.addEventListener('submit', (event) => { // from Mozilla 
+      event.preventDefault(); // prevents refresh of the page on the click event, so when the user inputs an email it maintains the list of users 
+      const inputEmail = email.value; // holds the value of the users email 
+      if (inputEmail) { // if the user input an email
+        const emailList = document.querySelector('#emailList'); // query for the email list variable
+        const listItem = document.createElement('li'); // create a list item to add to the list
+        listItem.textContent = inputEmail; // set the content (the users email) to be text
+        emailList.appendChild(listItem); // append the email to the list of emails 
+        email.value = ''; // reset the value when it has completed
     } else {
-        alert('No email provided. Please try again if you wish to be notified.');
+        alert('No email provided. Please try again if you wish to be notified.'); // did not put in an email, so give pop up that says they need to put an email in
     }
    
 });
